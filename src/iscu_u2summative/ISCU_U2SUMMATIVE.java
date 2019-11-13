d1bb1e4c8daa2689fe88e6c67d5fd5e51b223398
tyatk9075 <tyatk9075@7L0M6Y1.Student.UGDSB.ED>
2019-11-13 12:40:06 -0500

    Unit 2 Summative.

---
+++ src/iscu_u2summative/ISCU_U2SUMMATIVE.java
@@ -0,0 +1,126 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package iscu_u2summative;
+import java.util.Scanner;
+
+       
+/**
+ *
+ * @author tyatk9075
+ */
+public class ISCU_U2SUMMATIVE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+           Scanner keyedinput = new Scanner(System.in);
+          // Variable Declerations
+           int choice = 0;
+          double angle;
+           double radius;
+        
+            String word;
+           String letter;
+           String newLetter;
+                   
+                   //loop program for user conveneance
+                   while (choice !=6) {
+                    System.out.println  ("Would you like to:\n\n1: Find the area of a Triangle\n2: Find the circumference of a circle\n3: Find the Surface area of a sphere\n4: Find the SIN of a angle\n5: Find the area of a rectangle\n6: Exit");
+                   
+                    choice = keyedinput.nextInt();
+                   switch(choice){
+                case 1:
+                       System.out.println("Please enter the height of your triangle");
+                        double height = keyedinput.nextDouble();
+                        System.out.println("Please enter the base");
+                        double base = keyedinput.nextDouble();
+                      areaOfTriangle(height, base);
+                    break;
+                     case 2:
+                    System.out.println("Please enter the radius:");
+                    radius = keyedinput.nextDouble();
+                    areaOfCircle(radius);
+                    break;
+                          case 3:
+                    
+                    System.out.println("Please enter the radius:");
+                    radius = keyedinput.nextDouble();
+                    surAreaOfSphere(radius);
+                    break;
+                              
+                    case 4:
+                        System.out.println("Please enter the angle you woud like to convert");
+                        angle = keyedinput.nextDouble();
+                        sinOfAnAngle(angle);
+                       break;
+                    case 5:
+                       System.out.println("Enter the length of the rectangle");
+                        double length = keyedinput.nextDouble();
+                         System.out.println("Enter the width of the rectangle");
+                        double width = keyedinput.nextDouble();
+                        PeramaterOfARectangle(length, width);
+                        break;
+                         case 6:
+                    System.out.println("Exiting.");
+                    break;
+                        
+                            
+                              }
+    }
+    }
+    /**
+     * This method finds and outputs the area of a circle.
+     * @param rad 
+     */ 
+    // Area of a triangle method
+                  public static void areaOfTriangle(double height, double base){
+                       double area = base * height;
+                       area = area/2;
+                       System.out.println("The area is" +  area +"Squared units");
+                   }
+                  // sin of angle method
+                  public static void sinOfAnAngle (double angle){ 
+                      double sin = (Math.sin(angle));
+                      sin = (Math.round(sin));
+                      System.out.println("The sin of your angle is: " + sin +".");
+                  }
+                  // Surface area of a sphere
+                      public static void surAreaOfSphere(double rad){
+        double area = 4*Math.PI*Math.pow(rad, 2);
+        area = Math.round(area*100);
+        area/=100;
+        System.out.println("The surface area is " + area + " squared.");
+    }
+                 
+                  
+               
+                  // Area of a circle method
+                  public static void areaOfCircle(double radius){
+        double area = Math.PI*radius*radius;
+        area = (Math.round(area*100));
+        area/=100;
+        System.out.println("The area is " + area + " square units.");
+    }
+     /**
+     * This method finds and outputs the area of a rectangle.
+     * @param length
+     * @param width 
+     */
+                  // Area of a rectangle method
+    public static void PeramaterOfARectangle(double length, double width){
+        double area = length * width;
+       area = length * width;
+        area = Math.round(area*100);
+        area = 100;
+        System.out.println("The area is " + area + "Units.");
+    
+           
+           
+    }
+    
+}
